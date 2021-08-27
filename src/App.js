8234d443d8d2328bf014ea4bab379da6073e154b
Joseph Yekini <joseph@Josephs-MacBook-Pro.local>
2021-08-28 00:28:20 +0100

    update our menu items to navigate withrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router';
 import './App.css';
 
 import HomePage from './pages/homepage/Homepage.Component';
+const HatsPage = () => (
+  <div>
+     <h1>HatsPage</h1>
+  </div>
+) ;
 
 function App() {
   return (
     <div >
-    <HomePage />
-    
+       <switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/hats' component={HatsPage} />
+      </switch>
     </div>
   );
 }
